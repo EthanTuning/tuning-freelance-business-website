5affac23257572251e23d40c68dbf897b81c0b80
tuningfreelance <tuningfreelance@gmail.com>
2019-10-25 14:01:50 -0700

    app navbar is in, and other init stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule, MatGridList } from '@angular/material/grid-list';
+
 import { AppComponent } from './app.component';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
+
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
